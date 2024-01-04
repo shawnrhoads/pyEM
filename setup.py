cd702e0b2e68fe98fa396c4372466755602ad7cb
shawnrhoads <24925845+shawnrhoads@users.noreply.github.com>
2024-01-04 15:09:43 -0500

    removed requests from setup

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import find_packages, setup
-import requests
 
 # Get the repository owner and name from the GitHub URL
 github_url = 'https://github.com/shawnrhoads/pyEM'
-owner, repo = github_url.split('/')[-2:]
 
-# Get the list of contributors from the GitHub API
-response = requests.get(f'https://api.github.com/repos/{owner}/{repo}/contributors')
-contributors = response.json()
-
-# Create a list of author strings in the format "Name <email>"
-authors = [f"{c['login']}" for c in contributors]
+# Create a list of author strings
+authors = ["Shawn A. Rhoads"]
 
 # Get long description
 with open("README.md", "r") as fh:
