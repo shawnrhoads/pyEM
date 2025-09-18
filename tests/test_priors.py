20053c51a958422a259a218dd4b6a7df238e4e6f
Shawn Rhoads <24925845+shawnrhoads@users.noreply.github.com>
2025-09-18 12:46:46 -0400

    Update test to use GaussianPrior instead of UniformPrior
    Replaces UniformPrior with GaussianPrior in the test for prior and fit functionality. Adjusts the test to match the new prior, including parameter values and assertions. Removes the njobs argument from model.fit as well.

--- tests/test_priors.py
+++ tests/test_priors.py
@@ -1,7 +1,6 @@
 import numpy as np
-
 from pyem.api import EMModel
-from pyem.core.priors import UniformPrior
+from pyem.core.priors import GaussianPrior
 from pyem.models.rl import rw1a1b_fit as rw_fit, rw1a1b_simulate as rw_simulate
 
 
@@ -11,11 +10,10 @@ def test_uniform_prior_and_fit():
     sim = rw_simulate(params, nblocks=nblocks, ntrials=ntrials)
     all_data = [[c, r] for c, r in zip(sim["choices"], sim["rewards"])]
 
-    prior = UniformPrior(lower=[-1, -1], upper=[1, 1])
+    prior = GaussianPrior(mu=[0.0, 0.0], sigma=[100.0, 100.0])
     model = EMModel(all_data=all_data, fit_func=rw_fit, param_names=["beta", "alpha"])
-    result = model.fit(prior=prior, verbose=0, njobs=1)
+    result = model.fit(prior=prior, verbose=0)
 
     assert result.m.shape == (2, nsubjects)
     assert np.isfinite(prior.logpdf([0.0, 0.0]))
-    assert prior.logpdf([2.0, 0.0]) == -np.inf
 
